a771ed27c943cd75726253caa42df90f6e8f8817
Arman Vartan <arman.vartan@gmail.com>
2022-01-03 13:42:04 +0100

    Switched from fittest-selection to tournament-selection

--- src/main/java/de/socrates/paramecium/simulation/EvolutionStrategy.java
+++ src/main/java/de/socrates/paramecium/simulation/EvolutionStrategy.java
@@ -2,20 +2,95 @@ package de.socrates.paramecium.simulation;
 
 import de.socrates.paramecium.language.Instruction;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EvolutionStrategy {
 
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newWorkStealingPool();
+    private static final Comparator<Performance> BEST_PERFORMER = Comparator.comparing(Performance::getTicks).reversed();
     private final int programSize;
+    private final int sampleSize;
 
-    public EvolutionStrategy(int programSize) {
+    public EvolutionStrategy(int programSize, int sampleSize) {
         this.programSize = programSize;
+        this.sampleSize = sampleSize;
     }
 
-    public Program breed(Program mother, Program father) {
+    static Performance evaluate(Program program) {
+        return ProgramRunner.executeProgram(program);
+    }
+
+    static List<Performance> evaluate(List<Program> programs) {
+        List<Callable<Performance>> collect = programs.stream()
+                .map(p -> (Callable<Performance>) () -> evaluate(p))
+                .collect(Collectors.toList());
+
+        List<Performance> results = new ArrayList<>();
+        try {
+            List<Future<Performance>> futures = EXECUTOR_SERVICE.invokeAll(collect);
+            for (Future<Performance> future : futures) {
+                Performance performance = future.get();
+                results.add(performance);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return results;
+    }
+
+    List<Performance> selection(List<Performance> ancestors, List<Performance> descendants) {
+        List<Performance> collect = Stream.concat(ancestors.stream(), descendants.stream()).collect(Collectors.toList());
+
+        while (collect.size() > sampleSize) {
+            int i = selectRandomAncestor(collect);
+            int i2 = selectRandomAncestor(collect);
+
+            Performance o1 = collect.get(i);
+            Performance o2 = collect.get(i2);
+            switch (Comparator.comparing(Performance::getTicks).compare(o1, o2)) {
+                case 0:
+                case -1:
+                    collect.remove(i);
+                    break;
+                case +1:
+                    collect.remove(i2);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return collect;
+    }
+
+    List<Program> breed(List<Performance> ancestors) {
+        List<Program> descendants = new ArrayList<>();
+
+        for (Performance ancestor : ancestors) {
+            int index = selectRandomAncestor(ancestors);
+
+            Program mother = ancestor.getProgram();
+            Program father = ancestors.get(index).getProgram();
+            Program descendant = breed(mother, father);
+
+            descendants.add(descendant);
+        }
+
+        return descendants;
+    }
+
+    private Program breed(Program mother, Program father) {
         List<Instruction> motherCode = mother.getCode();
         List<Instruction> fatherCode = father.getCode();
 
@@ -27,6 +102,10 @@ public class EvolutionStrategy {
         ).collect(Collectors.toList());
 
         return new Program(descendant);
+    }
+
+    private static int selectRandomAncestor(List<Performance> ancestors) {
+        return ThreadLocalRandom.current().nextInt(0, ancestors.size());
     }
 
     private int randomProgramLine() {
--- src/main/java/de/socrates/paramecium/simulation/Simulation.java
+++ src/main/java/de/socrates/paramecium/simulation/Simulation.java
@@ -1,23 +1,12 @@
 package de.socrates.paramecium.simulation;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import picocli.CommandLine;
 
 public class Simulation implements Runnable {
-    private static final Comparator<Performance> BEST_PERFORMER = Comparator.comparing(Performance::getTicks).reversed();
-    private static final ExecutorService EXECUTOR_SERVICE = Executors.newWorkStealingPool();
 
     @CommandLine.Parameters
     private int programSize;
@@ -26,26 +15,28 @@ public class Simulation implements Runnable {
     @CommandLine.Parameters
     private int maxGeneration;
 
+    @CommandLine.Option(names = "renderbest")
+    private boolean renderBest = false;
+
     public static void main(String[] args) {
         System.exit(new CommandLine(new Simulation()).execute(args));
     }
 
     @Override
     public void run() {
+        EvolutionStrategy evolutionStrategy = new EvolutionStrategy(programSize, sampleSize);
+
         List<Performance> ancestors = init();
-
         for (int generation = 1; generation <= maxGeneration; generation++) {
-            List<Performance> descendants = evaluate(breed(ancestors));
+            List<Performance> descendants = EvolutionStrategy.evaluate(evolutionStrategy.breed(ancestors));
 
-            ancestors = Stream.concat(ancestors.stream(), descendants.stream())
-                    .sorted(BEST_PERFORMER)
-                    .limit(sampleSize)
-                    .collect(Collectors.toList());
+            ancestors = evolutionStrategy.selection(ancestors, descendants);
         }
 
         Performance best = ancestors.get(0);
 
-        ProgramRunner.executeProgram(best.getProgram(), true);
+        ProgramRunner.executeProgram(best.getProgram(), renderBest);
+        System.out.println(best.getProgram());
         System.out.println(best);
     }
 
@@ -54,47 +45,6 @@ public class Simulation implements Runnable {
         List<Program> init = IntStream.range(0, sampleSize)
                 .mapToObj(i -> programGenerator.randomProgram())
                 .collect(Collectors.toList());
-        return evaluate(init);
-    }
-
-    private List<Program> breed(List<Performance> ancestors) {
-        EvolutionStrategy evolutionStrategy = new EvolutionStrategy(programSize);
-
-        List<Program> descendants = new ArrayList<>();
-
-        for (Performance ancestor : ancestors) {
-            int index = ThreadLocalRandom.current().nextInt(0, ancestors.size());
-
-            Program mother = ancestor.getProgram();
-            Program father = ancestors.get(index).getProgram();
-            Program descendant = evolutionStrategy.breed(mother, father);
-
-            descendants.add(descendant);
-        }
-
-        return descendants;
-    }
-
-    private static Performance evaluate(Program program) {
-        return ProgramRunner.executeProgram(program);
-    }
-
-    private static List<Performance> evaluate(List<Program> programs) {
-        List<Callable<Performance>> collect = programs.stream()
-                .map(p -> (Callable<Performance>) () -> Simulation.evaluate(p))
-                .collect(Collectors.toList());
-
-        List<Performance> results = new ArrayList<>();
-        try {
-            List<Future<Performance>> futures = EXECUTOR_SERVICE.invokeAll(collect);
-            for (Future<Performance> future : futures) {
-                Performance performance = future.get();
-                results.add(performance);
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return results;
+        return EvolutionStrategy.evaluate(init);
     }
 }
