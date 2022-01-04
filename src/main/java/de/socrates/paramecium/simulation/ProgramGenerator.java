4b7034023021e7ac24d14f4be56d835d3433d05f
Arman Vartan <arman.vartan@gmail.com>
2022-01-04 01:19:42 +0100

    Simplified ProgramGenerator

--- src/main/java/de/socrates/paramecium/simulation/ProgramGenerator.java
+++ src/main/java/de/socrates/paramecium/simulation/ProgramGenerator.java
@@ -8,47 +8,38 @@ import de.socrates.paramecium.language.MoveInstruction;
 import de.socrates.paramecium.language.NopInstruction;
 
 import java.util.List;
-import java.util.Map;
 import java.util.SplittableRandom;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class ProgramGenerator {
 
     private final int programSize;
-    private final Map<Integer, Supplier<Instruction>> supplierMap;
+    private final List<Supplier<Instruction>> instructionSupplier;
 
     ProgramGenerator(int programSize) {
         this.programSize = programSize;
-        this.supplierMap = Map.of(
-                0, NopInstruction::new,
-                1, EatInstruction::new,
-                2, MoveInstruction::random,
-                3, this::generateGotoStatement,
-                4, this::generateIfClause);
+        this.instructionSupplier = List.of(
+                NopInstruction::new,
+                EatInstruction::new,
+                MoveInstruction::random,
+                this::generateGotoStatement,
+                this::generateIfClause);
     }
 
     Program randomProgram() {
-        List<Instruction> code = IntStream.range(0, programSize)
-                .mapToObj(i -> randomStatement())
+        List<Instruction> code = new SplittableRandom()
+                .ints(programSize, 0, instructionSupplier.size())
+                .mapToObj(instructionSupplier::get)
+                .map(Supplier::get)
                 .collect(Collectors.toList());
 
         return new Program(code);
     }
 
-    private Instruction randomStatement() {
-        int element = new SplittableRandom().nextInt(supplierMap.size());
-
-        return supplierMap.get(element).get();
-    }
-
-    private int randomLineNumber() {
-        return new SplittableRandom().nextInt(programSize);
-    }
-
     private Instruction generateGotoStatement() {
-        return new GotoInstruction(randomLineNumber());
+        int line = new SplittableRandom().nextInt(programSize);
+        return new GotoInstruction(line);
     }
 
     private Instruction generateIfClause() {
