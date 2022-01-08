d9c715f0bcf7908155fcf410e962f55af97266fc
Arman Vartan <arman.vartan@gmail.com>
2022-01-09 00:11:35 +0100

    Optimized selection

--- src/main/java/de/socrates/paramecium/simulation/Evolution.java
+++ src/main/java/de/socrates/paramecium/simulation/Evolution.java
@@ -1,11 +1,9 @@
 package de.socrates.paramecium.simulation;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Objects;
 import java.util.SplittableRandom;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,6 +15,7 @@ public class Evolution {
     private final int programSize;
 
     private final Statistics statistics;
+    private final SplittableRandom splittableRandom;
 
     public Evolution(EvolutionParameters evolutionParameters) {
         this.sampleSize = evolutionParameters.sampleSize;
@@ -24,6 +23,7 @@ public class Evolution {
         this.maxGeneration = evolutionParameters.maxGeneration;
         this.programSize = evolutionParameters.programSize;
         this.statistics = new Statistics();
+        this.splittableRandom = new SplittableRandom();
     }
 
     public void evolve() {
@@ -61,15 +61,15 @@ public class Evolution {
     }
 
     public List<Program> breed(List<Program> individuals) {
-        List<Program> collect = new SplittableRandom()
+        List<Program> mates = splittableRandom
                 .ints(individuals.size() * 2L, 0, individuals.size())
                 .mapToObj(individuals::get)
                 .collect(Collectors.toList());
 
         List<Program> descendants = new ArrayList<>();
-        for (int i = 0; i < collect.size(); i = i + 2) {
-            Program mother = collect.get(i);
-            Program father = collect.get(i + 1);
+        for (int i = 0; i < mates.size(); i = i + 2) {
+            Program mother = mates.get(i);
+            Program father = mates.get(i + 1);
             descendants.add(mother.mate(father));
             descendants.add(father.mate(mother));
         }
@@ -84,25 +84,27 @@ public class Evolution {
             return performances;
         }
 
-        Set<Integer> losers = new HashSet<>();
+        int losers = 0;
 
-        SplittableRandom splittableRandom = new SplittableRandom();
-        while (size - losers.size() > sampleSize) {
+        while (size - losers > sampleSize) {
             int i1 = splittableRandom.nextInt(size);
             int i2 = splittableRandom.nextInt(size);
 
-            if (!losers.contains(i1) && !losers.contains(i2)) {
-                int o1 = performances.get(i1).getTicks();
-                int o2 = performances.get(i2).getTicks();
+            Performance p1 = performances.get(i1);
+            Performance p2 = performances.get(i2);
 
-                losers.add(o1 <= o2 ? i1 : i2);
+            if (p1 != null && p2 != null) {
+                int o1 = p1.getTicks();
+                int o2 = p2.getTicks();
+
+                performances.set(o1 <= o2 ? i1 : i2, null);
+                losers++;
             }
         }
 
-        return IntStream.range(0, size)
-                .boxed()
-                .filter(Predicate.not(losers::contains))
-                .map(performances::get)
+        return performances.stream()
+                .parallel()
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
